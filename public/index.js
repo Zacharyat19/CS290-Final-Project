51b2d2e8518f7ad61b500c4d67b6d6e895a8c162
DSROKKAKU\Dominic <dominic@gnsd.net>
2021-06-02 13:23:07 -0700

    Added more formatting

--- public/index.js
+++ public/index.js
@@ -3,16 +3,16 @@ window.onload = randomize()
 function randomize() {
    
    console.log("pogent")
-   document.getElementById("firstMessage").style.top = (Math.floor(Math.random() * 100) + 10) + "px";
+   document.getElementById("firstMessage").style.top = (Math.floor(Math.random() * 100) + 10) + "%";
    console.log("first top " + document.getElementById("firstMessage").style.top)
-    document.getElementById("firstMessage").style.right = Math.floor(Math.random() * 100) + 10  + "px";
+    document.getElementById("firstMessage").style.right = Math.floor(Math.random() * 100) + 10  + "%";
 
-    document.getElementById("secondMessage").style.top = Math.floor(Math.random() * 100) + 10 + "px";
-    document.getElementById("secondMessage").style.left = Math.floor(Math.random() * 100) + 10 + "px";
+    document.getElementById("secondMessage").style.top = Math.floor(Math.random() * 100) + 10 + "%";
+    document.getElementById("secondMessage").style.left = Math.floor(Math.random() * 100) + 10 + "%";
 
-    document.getElementById("thirdMessage").style.bottom = Math.floor(Math.random() * 100) + 10 + "px";
-    document.getElementById("thirdMessage").style.right = Math.floor(Math.random() * 100) + 10 + "px";
+    document.getElementById("thirdMessage").style.bottom = Math.floor(Math.random() * 100) + 10 + "%";
+    document.getElementById("thirdMessage").style.right = Math.floor(Math.random() * 100) + 10 + "%";
 
-    document.getElementById("fourthMessage").style.bottom = Math.floor(Math.random() * 100) + 10 + "px";
-    document.getElementById("fourthMessage").style.left = Math.floor(Math.random() * 100) + 10 + "px";
+    document.getElementById("fourthMessage").style.bottom = Math.floor(Math.random() * 100) + 10 + "%";
+    document.getElementById("fourthMessage").style.left = Math.floor(Math.random() * 100) + 10 + "%";
 }
